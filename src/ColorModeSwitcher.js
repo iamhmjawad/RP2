a6ddc988736e81866f1b82e77acb49d4c64aafa6
iamhmjawad <thisishmjawad@gmail.com>
2023-01-28 14:14:26 +0500

    Jan 28/23

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import React from 'react'
+import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react'
+import { FaMoon, FaSun } from 'react-icons/fa'
 
-export const ColorModeSwitcher = props => {
-  const { toggleColorMode } = useColorMode();
-  const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+const ColorModeSwitcher = props => {
+  const { toggleColorMode } = useColorMode()
+  // const text = useColorModeValue('dark', 'light')
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
 
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
-      aria-label={`Switch to ${text} mode`}
+      // size="md"
+      // fontSize="lg"
+      // aria-label={`Switch to ${text} mode`}
+      // marginLeft="2"
+
+      // to place it on the top right corner
+      // 1 unit = 4px (default)
+      pos={'fixed'}
+      top="4"
+      right="4"
+
       variant="ghost"
       color="current"
-      marginLeft="2"
       onClick={toggleColorMode}
       icon={<SwitchIcon />}
       {...props}
     />
-  );
-};
+  )
+}
+
+export default ColorModeSwitcher
---
+++ src/components/Header.jsx
@@ -0,0 +1,106 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
+
+const Header = () => {
+  // to use these hooks, we need to import them from Chakra UI
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        pos={'fixed'}
+        top={'4'}
+        left={'4'}
+        colorScheme={'purple'}
+        p={0}
+        w={10}
+        h={10}
+        borderRadius={'full'}
+        // when the button is clicked, the onOpen function will be called
+        onClick={onOpen}
+      >
+        <BiMenuAltLeft />
+      </Button>
+
+      <Drawer placement={'left'} isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          {/* X button to close the drawer, calls onClose */}
+          <DrawerCloseButton />
+          <DrawerHeader>Video Hub</DrawerHeader>
+          <DrawerBody>
+            {/* basically display flex, v means verticall, elements will align vetically */}
+            <VStack alignItems={'flex-start'}>
+              {/* clicking this button will close the drawer */}
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/'}>Home</Link>
+              </Button>
+
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos?category=free'}>Free Videos</Link>
+              </Button>
+
+              <Button
+                onClick={onClose}
+                colorScheme={'purple'}
+                variant={'ghost'}
+              >
+                <Link to={'/'}>Upload Video</Link>
+              </Button>
+            </VStack>
+
+            {/* Hstack for Buttons in row */}
+            <HStack
+              pos={'absolute'}
+              left={0}
+              bottom={10}
+              w={'full'}
+              justifyContent={'space-evenly'}
+            >
+              <Button onClick={onClose} colorScheme={'purple'}>
+                <Link to={'/login'}>Log In</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                colorScheme={'purple'}
+                variant={'outline'}
+              >
+                <Link to={'/signup'}>Sign Up</Link>
+              </Button>
+            </HStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
